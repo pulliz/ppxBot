430b4a2e64c6c07529187fb8d4f57a9ea5a11aec
pulliz <108167025+pulliz@users.noreply.github.com>
2022-06-25 17:27:27 +0800

    Update send.js

--- fun/send.js
+++ fun/send.js
@@ -1,16 +1,24 @@
 const axios = require("axios")
 
 const {replyText} = require("./replyText")
-const {Token} = require('../config')
-const send = (params, isGroup) => {
+const {Token, AdministratorId, ChannelId} = require('../config')
+const send = (params, isGroup, tryAgain = true) => {
     axios.get(`https://api.telegram.org/bot${Token}/${isGroup ? 'sendMediaGroup' : 'sendVideo'}`, {
         params,
         timeout: 16000
     }).then(_ => {
         console.log(`视频${isGroup ? '组' : ''}发送成功`)
-    }).catch(() => {
+    }).catch(_ => {
         console.log(`视频${isGroup ? '组' : ''}超时错误`)
-        replyText(`视频${isGroup ? '组' : ''}上传至tg超时(16s),请重试${!isGroup ? '\n' + params['video'] : ''}`, params['chat_id'], '上传视频超时,发送链接')
+        if (tryAgain) {
+            send(params, isGroup, false)
+        } else {
+            let newParams = params
+            if (newParams['chat_id'] === ChannelId) {
+                newParams['chat_id'] = AdministratorId
+            }
+            replyText(`视频${isGroup ? '组' : ''}上传至tg超时(16s),请重试${!isGroup ? '\n' + params['video'] : ''}`, newParams['chat_id'], '上传视频超时,发送链接')
+        }
     })
 }
 
